05647b0412afb4f5e39ff61feebdc2d5f979c934
Jack Sunderman <jacksunderman@Jacks-MacBook-Air.local>
2023-09-01 11:38:29 -0400

    display selected puppy kind of

--- spa.js
+++ spa.js
@@ -1,18 +1,68 @@
 const allPuppies = document.getElementById("allPuppies")
+const selectedPuppy = document.getElementById("selectedPuppy")
 
+let puppies= [];
 
 async function fetchData () {
-    const allData = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2307-ftb-et-web-ft/players')
+    const allData = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2307/players')
     const json = await allData.json()
-    const puppies =json.data 
+    console.log(json)
+    puppies =json.data.players
     console.log(puppies)
+    
+    
+    
 }
 
-function render() {
-    const htmlPuppies = puppies.map((id) = {
-        return `<h3> ${} </h3>`
+
+async function render(){
+    await fetchData()
+    const hash = window.location.hash.slice(1)
+    console.log(hash)
+
+    const puppiesHtml = puppies.map((puppy) => {
+        
+        return `<li> <a href="#${puppy.id}" class="${puppy.id === hash ? 'selected' : ''}"> <h3>Puppy: ${puppy.id}</h3></a> </li>
+        `
     })
+    allPuppies.innerHTML = puppiesHtml.join('')
+
+    let puppySelection = puppies.find((puppy)=>{
+        return puppy.id = hash
+        return puppySelection.push(puppy)
+        
+    })
+    
+    let hashPuppy = [];
+    hashPuppy.push(puppySelection);
+    console.log(hashPuppy)
+    
+    selectedPuppyHtml = hashPuppy.map((info) =>{
+        return `Name: ${info.name} <br>
+            Breed: ${info.breed} <br>
+            <img src ="${info.imageUrl}">
+            `
+    })
+    selectedPuppy.innerHTML = selectedPuppyHtml.join('')
+
+    
+
+    
+    
 }
 
-fetchData()
+
+
+window.addEventListener("hashchange", () => {
+    
+    render()
+    
+    
+})
+render()
+
+
+
+
+
 
